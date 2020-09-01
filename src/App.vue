bee83414367e3fac45b1459032e55bee2f638a97
hukai <huzhengen@163.com>
2020-09-01 09:24:47 +0800

    button card image

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Button />
+    <Card />
+    <image1 />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from "./components/Card.vue";
+import Image from "./components/Image.vue";
+import Button from "./components/Button.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Card,
+    Image1: Image,
+    Button,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Button, Card, Image, Row, } from "element-ui";
+Vue.use(Button).use(Card).use(Image).use(Row)
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
 
